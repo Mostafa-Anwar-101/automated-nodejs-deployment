79dba2545c65aa58fbe7c6c0f74e0d42743ad6ff
Mostafa-Anwar-101 <mostafa.mohamed.anwar13@gmail.com>
2025-05-24 21:41:02 +0300

    feat: add jenkins slave as private instances

--- terraform/modules/private_instances/main.tf
+++ terraform/modules/private_instances/main.tf
@@ -13,3 +13,18 @@ resource "aws_instance" "private" {
     Name = "${var.project_name}-${each.key}-private-instance"
   }
 }
+
+resource "aws_instance" "jenkins_slave" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.private_subnets["private-1"].subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_name
+
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "${var.project_name}-jenkins-slave"
+  }
+  
+}
--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -77,7 +77,7 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public["public-1"].id
-  tags          = { Name = "main-nat-gateway" }
+  tags          = { Name = "${var.project_name}-gateway" }
 
   depends_on = [aws_internet_gateway.igw]
 }
